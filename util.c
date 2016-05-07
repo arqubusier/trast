be95ae5e34772433706bf80fa7a4d4de6c1d914c
Herman Lundkvist <herlu184@student.liu.se>
2016-05-08 01:30:09 +0200

    wrote percent encode

--- util.c
+++ util.c
@@ -1,4 +1,6 @@
 #include "util.h"
+#include <string.h>
+#include <stdio.h>
 
 int n_digits(int i){
     if (i < 0) i = -i + 1;
@@ -21,3 +23,39 @@ size_t strcpy_limit(char* dst, const char* src, size_t limit){
     }
     return i;
 };
+
+size_t percent_encode_size(const char* src){
+    size_t src_sz = strlen(src);
+    int i=0, total_size=0;
+    for (; i < src_sz; i++){
+        char c = src[i];
+        if ((c >= 0x30 && c <= 0x39) || 
+                (c >= 0x41 && c <= 0x5A) ||
+                (c >= 0x61 && c <= 0x7A) ||
+                c==0x2D || c==0x2E || c == 0x5F || c==0x7E){
+            total_size += 1;
+        }
+        else{
+            total_size += 3;
+        }
+    }
+}
+
+void percent_encode(char *dst, const char *src){
+    size_t src_sz = strlen(src);
+    int i=0, offs=0, total_size=0;
+    for (; i < src_sz; i++){
+        char c = src[i];
+        if ((c >= 0x30 && c <= 0x39) || 
+                (c >= 0x41 && c <= 0x5A) ||
+                (c >= 0x61 && c <= 0x7A) ||
+                c==0x2D || c==0x2E || c == 0x5F || c==0x7E){
+            dst[offs] = src[i];
+            offs += 1;
+        }
+        else{
+            sprintf(&dst[offs], "%d2x", src[i]);
+            offs += 3;
+        }
+    }
+}
