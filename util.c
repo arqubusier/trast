ad4731c53613153647f08aebd40b47a12a645504
Herman Lundkvist <herman.lundkvist@gmail.com>
2016-07-31 21:02:51 +0200

    added util files

--- util.c
+++ util.c
@@ -4,9 +4,12 @@
 #include "esp/hwrand.h"
 #include "sys/types.h"
 
-const char BASE64_ALPHABET0[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-const char ALPHA_NUM[] = BASE64_ALPHABET;
+
+const char BASE64_ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const char ALPHA_NUM[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 const int  ALPHA_NUM_LEN = 62;
+
+#define BASE64(i) BASE64_ALPHABET[i] 
 
 int n_digits(int i){
     if (i < 0) i = -i + 1;
@@ -52,7 +55,7 @@ size_t percent_encode_size(const char* src){
 
 void percent_encode(char *dst, const char *src){
     size_t src_sz = strlen(src);
-    int i=0, offs=0, total_size=0;
+    int i=0, offs=0;
     for (; i < src_sz; i++){
         char c = src[i];
         if ((c >= 0x30 && c <= 0x39) || 
@@ -83,21 +86,42 @@ size_t base64_pad_len(const int src_size){
  *    -------------------------------------------------------------------------
  *    0  1  2  3  4  5   6  7  0  1  2  3   4  5  6  7  0  1   2  3  4  5  6  7 
  */
-
-void base64_encode(unsigned char* dst, size_t dst_len,
-                   unsigned char* src, size_t src_len){
+void base64_encode(char* dst, size_t dst_len,
+                   const unsigned char* src, size_t src_len){
     int dst_offs = 0, src_offs = 0;
-    if (dst_len > 0){
-        dst[dst_len-2] = '=';
-        dst[dst_len-1] = '=';
-        dst[dst_len-0] = '=';
+    uint pad = BASE64_PAD(src_len);
+
+    printf("src %02X %02X %02X\n", src[0], src[1], src[2]);
+    for(; src_offs < src_len-pad; src_offs+=3, dst_offs+=4){
+        printf("src %02X %02X %02X\n", src[src_offs], src[src_offs+1], src[src_offs+2]);
+        dst[dst_offs] = BASE64(src[src_offs] >> 2);
+        printf("%d\n", dst[dst_offs]);
+        dst[dst_offs + 1] = BASE64(
+            0x3F & ((src[src_offs] << 4) | (src[src_offs + 1] >> 4)));
+        dst[dst_offs + 2] = BASE64(
+            0x3F & ((src[src_offs + 1] << 2) | (src[src_offs + 2] >> 6)));
+        dst[dst_offs + 3] = BASE64(src[src_offs+2] & 0x3F);
     }
 
-    for(; src_offs < src_len; src_offs+=3, dst_offs+=4){
-        dst[dst_offs] = src[src_offs] >> 2;
-        dst[dst_offs + 1] = (src[src_offs] << 6) & (src[src_offs] >> 4);
-        dst[dst_offs + 2] = (src[src_offs] << 4) & (src[src_offs] >> 6);
-        dst[dst_offs + 3] = src[src_offs] >> 2 & 0xC0;
+
+    if (pad > 0){
+        int i=0, src_offs=src_len-3;
+        char remainder[3] = {0, 0, 0};
+        for (;i < src_len%3; i++){
+            remainder[i] = src[src_offs + i];
+        }
+
+        dst[dst_offs] = BASE64(remainder[0] >> 2);
+        dst[dst_offs + 1] = BASE64(
+            0x3F & ((remainder[0] << 4) | (remainder[1] >> 4)));
+        dst[dst_offs + 2] = BASE64(
+            0x3F & ((remainder[1] << 2) | (remainder[2] >> 6)));
+        dst[dst_offs + 3] = BASE64(remainder[2] & 0x3F);
+
+
+        for (i=1 ;i <= pad; i++){
+            dst[dst_len-i] = '=';
+        }
     }
 }
 
@@ -114,4 +138,14 @@ void alpha_num_rand(char* buf, const int buf_len){
     for (;i < buf_len; i++){
         buf[i] =  ALPHA_NUM[buf[i] % ALPHA_NUM_LEN];
     }
+}
+
+void print_hex(unsigned char* buf, uint buf_len){
+    int i=0;
+    for (i = 0; i < buf_len; i++)
+    {
+        if (i > 0) printf(":");
+        printf("%02X", buf[i]);
+    }
+    printf("\n");
 }
