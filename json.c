97c8354d14e8607719c45ddc48d255ac78138cb7
Herman Lundkvist <herman.lundkvist@gmail.com>
2016-08-23 23:45:28 +0200

    parser midway

--- json.c
+++ json.c
@@ -1,311 +1,395 @@
-#include "jsmn.h"
+#include "json.h"
 
-/**
- * Allocates a fresh unused token from the token pull.
- */
-static jsmntok_t *jsmn_alloc_token(jsmn_parser *parser,
-		jsmntok_t *tokens, size_t num_tokens) {
-	jsmntok_t *tok;
-	if (parser->toknext >= num_tokens) {
-		return NULL;
-	}
-	tok = &tokens[parser->toknext++];
-	tok->start = tok->end = -1;
-	tok->size = 0;
-#ifdef JSMN_PARENT_LINKS
-	tok->parent = -1;
-#endif
-	return tok;
-}
 
-/**
- * Fills token type and boundaries.
- */
-static void jsmn_fill_token(jsmntok_t *token, jsmntype_t type,
-                            int start, int end) {
-	token->type = type;
-	token->start = start;
-	token->end = end;
-	token->size = 0;
-}
-
-/**
- * Fills next available token with JSON primitive.
- */
-static int jsmn_parse_primitive(jsmn_parser *parser, const char *js,
+/*
+static int _parse_primitive(jsmn_parser *parser, const char *js,
 		size_t len, jsmntok_t *tokens, size_t num_tokens) {
 	jsmntok_t *token;
 	int start;
 
-	start = parser->pos;
+	start = parser->buff_offset;
 
-	for (; parser->pos < len && js[parser->pos] != '\0'; parser->pos++) {
-		switch (js[parser->pos]) {
+	for (; parser->buff_offset < len && js[parser->buff_offset] != '\0'; parser->buff_offset++) {
+		switch (js[parser->buff_offset]) {
 #ifndef JSMN_STRICT
-			/* In strict mode primitive must be followed by "," or "}" or "]" */
 			case ':':
 #endif
 			case '\t' : case '\r' : case '\n' : case ' ' :
 			case ','  : case ']'  : case '}' :
 				goto found;
 		}
-		if (js[parser->pos] < 32 || js[parser->pos] >= 127) {
-			parser->pos = start;
+		if (js[parser->buff_offset] < 32 || js[parser->buff_offset] >= 127) {
+			parser->buff_offset = start;
 			return JSMN_ERROR_INVAL;
 		}
 	}
 #ifdef JSMN_STRICT
-	/* In strict mode primitive must be followed by a comma/object/array */
-	parser->pos = start;
+	parser->buff_offset = start;
 	return JSMN_ERROR_PART;
 #endif
 
 found:
 	if (tokens == NULL) {
-		parser->pos--;
+		parser->buff_offset--;
 		return 0;
 	}
 	token = jsmn_alloc_token(parser, tokens, num_tokens);
 	if (token == NULL) {
-		parser->pos = start;
+		parser->buff_offset = start;
 		return JSMN_ERROR_NOMEM;
 	}
-	jsmn_fill_token(token, JSMN_PRIMITIVE, start, parser->pos);
+	jsmn_fill_token(token, JSMN_PRIMITIVE, start, parser->buff_offset);
 #ifdef JSMN_PARENT_LINKS
 	token->parent = parser->toksuper;
 #endif
-	parser->pos--;
+	parser->buff_offset--;
 	return 0;
 }
+*/
 
 /**
  * Fills next token with JSON string.
  */
-static int jsmn_parse_string(jsmn_parser *parser, const char *js,
-		size_t len, jsmntok_t *tokens, size_t num_tokens) {
-	jsmntok_t *token;
+//static int jsmn_parse_string(jsmn_parser *parser, const char *js,
+//		size_t len, jsmntok_t *tokens, size_t num_tokens) {
+//	jsmntok_t *token;
+//
+//	int start = parser->buff_offset;
+//
+//	parser->buff_offset++;
+//
+//	/* Skip starting quote */
+//	for (; parser->buff_offset < len && js[parser->buff_offset] != '\0'; parser->buff_offset++) {
+//		char c = js[parser->buff_offset];
+//
+//		/* Quote: end of string */
+//		if (c == '\"') {
+//			if (tokens == NULL) {
+//				return 0;
+//			}
+//			token = jsmn_alloc_token(parser, tokens, num_tokens);
+//			if (token == NULL) {
+//				parser->buff_offset = start;
+//				return JSMN_ERROR_NOMEM;
+//			}
+//			jsmn_fill_token(token, JSMN_STRING, start+1, parser->buff_offset);
+//#ifdef JSMN_PARENT_LINKS
+//			token->parent = parser->toksuper;
+//#endif
+//			return 0;
+//		}
+//
+//		/* Backslash: Quoted symbol expected */
+//		if (c == '\\' && parser->buff_offset + 1 < len) {
+//			int i;
+//			parser->buff_offset++;
+//			switch (js[parser->buff_offset]) {
+//				/* Allowed escaped symbols */
+//				case '\"': case '/' : case '\\' : case 'b' :
+//				case 'f' : case 'r' : case 'n'  : case 't' :
+//					break;
+//				/* Allows escaped symbol \uXXXX */
+//				case 'u':
+//					parser->buff_offset++;
+//					for(i = 0; i < 4 && parser->buff_offset < len && js[parser->buff_offset] != '\0'; i++) {
+//						/* If it isn't a hex character we have an error */
+//						if(!((js[parser->buff_offset] >= 48 && js[parser->buff_offset] <= 57) || /* 0-9 */
+//									(js[parser->buff_offset] >= 65 && js[parser->buff_offset] <= 70) || /* A-F */
+//									(js[parser->buff_offset] >= 97 && js[parser->buff_offset] <= 102))) { /* a-f */
+//							parser->buff_offset = start;
+//							return JSMN_ERROR_INVAL;
+//						}
+//						parser->buff_offset++;
+//					}
+//					parser->buff_offset--;
+//					break;
+//				/* Unexpected symbol */
+//				default:
+//					parser->buff_offset = start;
+//					return JSMN_ERROR_INVAL;
+//			}
+//		}
+//	}
+//	parser->buff_offset = start;
+//	return JSMN_ERROR_PART;
+//}
+//
 
-	int start = parser->pos;
-
-	parser->pos++;
-
-	/* Skip starting quote */
-	for (; parser->pos < len && js[parser->pos] != '\0'; parser->pos++) {
-		char c = js[parser->pos];
-
-		/* Quote: end of string */
-		if (c == '\"') {
-			if (tokens == NULL) {
-				return 0;
-			}
-			token = jsmn_alloc_token(parser, tokens, num_tokens);
-			if (token == NULL) {
-				parser->pos = start;
-				return JSMN_ERROR_NOMEM;
-			}
-			jsmn_fill_token(token, JSMN_STRING, start+1, parser->pos);
-#ifdef JSMN_PARENT_LINKS
-			token->parent = parser->toksuper;
-#endif
-			return 0;
-		}
-
-		/* Backslash: Quoted symbol expected */
-		if (c == '\\' && parser->pos + 1 < len) {
-			int i;
-			parser->pos++;
-			switch (js[parser->pos]) {
-				/* Allowed escaped symbols */
-				case '\"': case '/' : case '\\' : case 'b' :
-				case 'f' : case 'r' : case 'n'  : case 't' :
-					break;
-				/* Allows escaped symbol \uXXXX */
-				case 'u':
-					parser->pos++;
-					for(i = 0; i < 4 && parser->pos < len && js[parser->pos] != '\0'; i++) {
-						/* If it isn't a hex character we have an error */
-						if(!((js[parser->pos] >= 48 && js[parser->pos] <= 57) || /* 0-9 */
-									(js[parser->pos] >= 65 && js[parser->pos] <= 70) || /* A-F */
-									(js[parser->pos] >= 97 && js[parser->pos] <= 102))) { /* a-f */
-							parser->pos = start;
-							return JSMN_ERROR_INVAL;
-						}
-						parser->pos++;
-					}
-					parser->pos--;
-					break;
-				/* Unexpected symbol */
-				default:
-					parser->pos = start;
-					return JSMN_ERROR_INVAL;
-			}
-		}
-	}
-	parser->pos = start;
-	return JSMN_ERROR_PART;
+enum json_state{
+    JSON_STATE_BEGIN_VAL_OR_KEY,
+    JSON_STATE_INT_ELEM,
+    JSON_STATE_TRUE_ELEM,
+    JSON_STATE_FALSE_ELEM,
+    JSON_STATE_NULL_ELEM,
+    JSON_STATE_KEY_OR_STR,
+    JSON_STATE_KEY_OR_STR_ESCAPE,
+    JSON_STATE_KEY_OR_STR_END,
+    JSON_STATE_TYPE,
+    JSON_STATE_INT,
+    JSON_STATE_SPECIAL,
+    JSON_STATE_STR,
+    JSON_STATE_STR_ESCAPE,
+    JSON_STATE_END_STR,
+    JSON_STATE_END_VAL
 }
 
-/**
- * Parse JSON string and fill tokens.
- */
-int jsmn_parse(jsmn_parser *parser, const char *js, size_t len,
-		jsmntok_t *tokens, unsigned int num_tokens) {
-	int r;
+/*----------------------------------------------------------------------------*
+ * State handling functions
+ *----------------------------------------------------------------------------*/
+const char *const TRUE_VAL[] = "true";
+const char *const FALSE_VAL[] = "false";
+const char *const NULL_VAL[] = "null";
+const size_t TRUE_VAL_LEN = 4;
+const size_t FALSE_VAL_LEN = 5;
+const size_t NULL_VAL_LEN = 4;
+
+inline bool is_special(const size_t offs, const char c, int state){
+    return ( (state == JSON_STATE_TRUE_ELEM && offs < TRUE_VAL_LEN
+              && c == TRUE_VAL[offs])
+          || (state == JSON_STATE_NULL_ELEM && offs < NULL_VAL_LEN
+              && c == NULL_VAL[offs])
+          || (state == JSON_STATE_FALSE_ELEM && offs < FALSE_VAL_LEN
+              && c == FALSE_VAL[offs]);
+        
+}
+
+int handle_begin_value_or_key(json_parser *parser, const char c){
+    switch (c){
+        case '{': case '[':
+            parser->level++;
+            break;
+        case '"':
+            parser->state = JSON_STATE_KEY_OR_STR;
+            break;
+        case '0' ... '9':
+            parser->state = JSON_STATE_INT_ELEM;
+            break;
+        case 't', 'f', 'n':
+            parser->state = JSON_STATE_SPECIAL_ELEM;
+            break;
+        //skip White spaces
+        case ' ': case '\t': case '\r': case '\n': case: '\t':
+            break;
+        default:
+            return JSON_ERROR_PARSING;
+    }
+    return JSON_CONTINUE;
+}
+
+int handle_int_elem(json_parser *parser, const char c){
+    switch (c){
+        //TODO: add matching inside arrays
+        case '0' ... '9':
+            break;
+        case ']':
+            parser->level--;
+            parser->state = JSON_STATE_VAL_END;
+            break;
+        case ',':
+            parser->state = JSON_STATE_BEGIN_VAL_OR_KEY;
+            break;
+        default:
+            return JSON_ERROR_PARSING;
+    }
+    return JSON_CONTINUE;
+
+}
+
+int handle_special_elem(json_parser *parser, const char c){
+    switch (c){
+        case ']':
+            parser->level--;
+            parser->state = JSON_STATE_VAL_END;
+            parser->key_offset = 0;
+            break;
+        case ',':
+            parser->state = JSON_STATE_BEGIN_VAL_OR_KEY;
+            parser->key_offset = 0;
+            break;
+        default:
+            if (!is_special(parser->key_offset, c, parser->state))
+                return JSON_ERROR_PARSING;
+            parser->key_offset++;
+    }
+    return JSON_CONTINUE;
+}
+
+//ADD SAVE TO STORAGE
+int handle_key_or_str(json_parser *parser, const char c){
+    int i;
+    json_int_t *prev_int_match = int_matches;
+    json_str_t *prev_str_match = str_matches;
+    json_int_t *int_match;
+    json_str_t *str_match;
+
+    switch (c){
+        case '\\':
+            parser->state = JSON_STATE_KEY_OR_STR_ESCAPE;
+            parser->key_offset++;
+            break;
+        case '"':
+            parser->state = JSON_STATE_KEY_OR_STR_END;
+            //mark matches
+            for (int_match = parser->int_matches; int_match != JSON_LIST_EMPTY;
+                    int_match = int_match->next){
+                int_match->match_upto_level++;
+            }
+
+            parser->key_offset = 0;
+        default:
+            //fill the list of matches
+            if (parser->should_find_matches){
+                for (i=0; i< parser->n_ints; ++i){
+                    // Note that keys must contain at least one char
+                    // namely '\0'
+                    int_match = &int_matches[i];
+                    if ((int_match->match_upto_level == parser->level - 1)
+                            && (int_match->max_level <= parser->level)
+                            && (c == int_match->key_addr[parser->level][0])){
+                        prev_int_match->next = int_match;
+                        prev_int_match = int_match;
+                    }
+                }
+            }
+            //filter out key addresses that don't match
+            else{
+                int_match = parser->int_matches;
+                prev_int_match = int_match;
+                for (;int_match != JSON_LIST_EMPTY;
+                       int_match = int_match->next){
+                    if (c != int_match->key_addr[parser->level][key_offset])
+                        if (prev_int_match == parser->int_matches)
+                            parser->int_matches = int_match->next;
+                        else
+                            prev_int_match->next = int_match->next;
+                    else
+                        prev_int_match = int_match;
+                }
+            }
+
+            parser->key_offset++;
+    }
+}
+
+//TODO ADD OPTIMISATIONS DURING KEY MATCHING
+int json_parse(json_parser *parser,
+               const char *input_buff, size_t buff_sz);
 	int i;
-	jsmntok_t *token;
-	int count = parser->toknext;
 
-	for (; parser->pos < len && js[parser->pos] != '\0'; parser->pos++) {
+    //The parser looks at one char at a time.
+    //the character is only advanced by this foor loop.
+	for (; parser->buff_offset < buff_sz && js[parser->buff_offset] != '\0';
+           parser->buff_offset++) {
 		char c;
-		jsmntype_t type;
+        int res;
 
-		c = js[parser->pos];
-		switch (c) {
-			case '{': case '[':
-				count++;
-				if (tokens == NULL) {
-					break;
-				}
-				token = jsmn_alloc_token(parser, tokens, num_tokens);
-				if (token == NULL)
-					return JSMN_ERROR_NOMEM;
-				if (parser->toksuper != -1) {
-					tokens[parser->toksuper].size++;
-#ifdef JSMN_PARENT_LINKS
-					token->parent = parser->toksuper;
-#endif
-				}
-				token->type = (c == '{' ? JSMN_OBJECT : JSMN_ARRAY);
-				token->start = parser->pos;
-				parser->toksuper = parser->toknext - 1;
-				break;
-			case '}': case ']':
-				if (tokens == NULL)
-					break;
-				type = (c == '}' ? JSMN_OBJECT : JSMN_ARRAY);
-#ifdef JSMN_PARENT_LINKS
-				if (parser->toknext < 1) {
-					return JSMN_ERROR_INVAL;
-				}
-				token = &tokens[parser->toknext - 1];
-				for (;;) {
-					if (token->start != -1 && token->end == -1) {
-						if (token->type != type) {
-							return JSMN_ERROR_INVAL;
-						}
-						token->end = parser->pos + 1;
-						parser->toksuper = token->parent;
-						break;
-					}
-					if (token->parent == -1) {
-						break;
-					}
-					token = &tokens[token->parent];
-				}
-#else
-				for (i = parser->toknext - 1; i >= 0; i--) {
-					token = &tokens[i];
-					if (token->start != -1 && token->end == -1) {
-						if (token->type != type) {
-							return JSMN_ERROR_INVAL;
-						}
-						parser->toksuper = -1;
-						token->end = parser->pos + 1;
-						break;
-					}
-				}
-				/* Error if unmatched closing bracket */
-				if (i == -1) return JSMN_ERROR_INVAL;
-				for (; i >= 0; i--) {
-					token = &tokens[i];
-					if (token->start != -1 && token->end == -1) {
-						parser->toksuper = i;
-						break;
-					}
-				}
-#endif
-				break;
-			case '\"':
-				r = jsmn_parse_string(parser, js, len, tokens, num_tokens);
-				if (r < 0) return r;
-				count++;
-				if (parser->toksuper != -1 && tokens != NULL)
-					tokens[parser->toksuper].size++;
-				break;
-			case '\t' : case '\r' : case '\n' : case ' ':
-				break;
-			case ':':
-				parser->toksuper = parser->toknext - 1;
-				break;
-			case ',':
-				if (tokens != NULL && parser->toksuper != -1 &&
-						tokens[parser->toksuper].type != JSMN_ARRAY &&
-						tokens[parser->toksuper].type != JSMN_OBJECT) {
-#ifdef JSMN_PARENT_LINKS
-					parser->toksuper = tokens[parser->toksuper].parent;
-#else
-					for (i = parser->toknext - 1; i >= 0; i--) {
-						if (tokens[i].type == JSMN_ARRAY || tokens[i].type == JSMN_OBJECT) {
-							if (tokens[i].start != -1 && tokens[i].end == -1) {
-								parser->toksuper = i;
-								break;
-							}
-						}
-					}
-#endif
-				}
-				break;
-#ifdef JSMN_STRICT
-			/* In strict mode primitives are: numbers and booleans */
-			case '-': case '0': case '1' : case '2': case '3' : case '4':
-			case '5': case '6': case '7' : case '8': case '9':
-			case 't': case 'f': case 'n' :
-				/* And they must not be keys of the object */
-				if (tokens != NULL && parser->toksuper != -1) {
-					jsmntok_t *t = &tokens[parser->toksuper];
-					if (t->type == JSMN_OBJECT ||
-							(t->type == JSMN_STRING && t->size != 0)) {
-						return JSMN_ERROR_INVAL;
-					}
-				}
-#else
-			/* In non-strict mode every unquoted value is a primitive */
-			default:
-#endif
-				r = jsmn_parse_primitive(parser, js, len, tokens, num_tokens);
-				if (r < 0) return r;
-				count++;
-				if (parser->toksuper != -1 && tokens != NULL)
-					tokens[parser->toksuper].size++;
-				break;
+		c = js[parser->offset];
 
-#ifdef JSMN_STRICT
-			/* Unexpected char in strict mode */
-			default:
-				return JSMN_ERROR_INVAL;
-#endif
-		}
+
+        switch (parser->state){
+            case JSON_STATE_BEGIN_VAL_OR_KEY:
+                res = handle_begin_val_or_key(parser, c); break;
+            case JSON_STATE_INT_ELEM:
+                res = handle_int_elem(parser, c); break;
+            case JSON_STATE_TRUE_ELEM: case JSON_STATE_FALSE_ELEM:
+                    case JSON_STATE_NULL_ELEM;
+                res = handle_special_elem(parser, c); break;
+            case JSON_STATE_KEY_OR_STR:
+                res = handle_key_or_str(parser, c); break;
+            case JSON_STATE_KEY_OR_STR_ESCAPE:
+                res = handle_key_or_str_escape(parser, c); break;
+            case JSON_STATE_KEY_OR_STR_END:
+                res = handle_key_or_str_end(parser, c); break;
+            case JSON_STATE_TYPE:
+                res = handle_type(parser, c); break;
+            case JSON_STATE_INT:
+                res = handle_int(parser, c); break;
+            case JSON_STATE_SPECIAL:
+                res = handle_special(parser, c); break;
+            case JSON_STATE_STR:
+                res = handle_str(parser, c); break;
+            case JSON_STATE_STR_ESCAPE:
+                res = handle_str_escape(parser, c); break;
+            case JSON_STATE_END_STR:
+                res = handle_end_str(parser, c); break;
+            case JSON_END_VAL:
+                res = handle_end_val(parser, c); break;
+            default:
+                return JSON_PARSER_FAULT;
+
+            //parsing stop if we are done or if there was an error
+            if (res <= 0)
+                return res;
+        }
 	}
 
-	if (tokens != NULL) {
-		for (i = parser->toknext - 1; i >= 0; i--) {
-			/* Unmatched opened object or array */
-			if (tokens[i].start != -1 && tokens[i].end == -1) {
-				return JSMN_ERROR_PART;
-			}
-		}
-	}
-
-	return count;
+    //ran out of buffer, parser is either done or needs more input
+    if (level == 0)
+        return JSON_PARSING_DONE;
+    return JSON_CONTINUE;
 }
 
-/**
- * Creates a new parser based over a given  buffer with an array of tokens
- * available.
- */
-void jsmn_init(jsmn_parser *parser) {
-	parser->pos = 0;
-	parser->toknext = 0;
-	parser->toksuper = -1;
+void json_parser_init(json_parser *parser, json_int_t *ints, size_t n_ints,
+                      json_str_t *strs, size_t n_strs){
+	parser->level = 0;
+	parser->buff_offset = 0;
+    parser->state = JSON_STATE_VAL;
+    parser->int_matches = JSON_LIST_EMPTY;
+    parser->n_ints = n_ints;
+    parser->str_matches = JSON_LIST_EMPTY;
+    parser->n_strs = n_strs;
 }
 
+void json_ints_init(json_int_t *values, const char **keys_addrs[],
+                    int *storages, size_t n_values){
+    size_t i;
+    if (n_vals == 0)
+        return;
+
+    json_int_t *val; 
+    for(i=0; i<n_vals - 1; ++i){
+        val = &values[i];
+        val->key_addr = key_addrs[i];
+        val->storage = storages[i];
+        val->max_levels = 
+        val->match =0;
+        val->next = &values[i+1];
+    }
+
+    /* NULL marks the end of the chained list */
+    i = n_vals - 1;
+    val = &values[i];
+    val->key_addr = key_addrs[i];
+    val->storage = storages[i];
+    val->max_levels = 
+    val->match = 0;
+    val->next = NULL;
+}
+
+void json_str_init(json_str_t *values, const char **keys_addrs[],
+                    int *storages, size_t n_values){
+    size_t i;
+    if (n_vals == 0)
+        return;
+
+    json_str_t *val; 
+    for(i=0; i<n_vals - 1; ++i){
+        val = &values[i];
+        val->key_addr = key_addrs[i];
+        val->storage = storages[i];
+        val->storage_sz = 
+        val->max_levels = 
+        val->match = 0;
+        val->next = &values[i+1];
+    }
+
+    /* NULL marks the end of the chained list */
+    i = n_vals - 1;
+    val = &values[i];
+    val->key_addr = key_addrs[i];
+    val->storage = storages[i];
+    val->storage_sz = 
+    val->max_levels = 
+    val->match = 0;
+    val->next = NULL;
+}
--- json.h
+++ json.h
@@ -1,5 +1,5 @@
-#ifndef __JSMN_H_
-#define __JSMN_H_
+#ifndef __JSON_H_
+#define __JSON_H_
 
 #include <stddef.h>
 
@@ -7,70 +7,69 @@
 extern "C" {
 #endif
 
-/**
- * JSON type identifier. Basic types are:
- * 	o Object
- * 	o Array
- * 	o String
- * 	o Other primitive: number, boolean (true/false) or null
- */
-typedef enum {
-	JSMN_UNDEFINED = 0,
-	JSMN_OBJECT = 1,
-	JSMN_ARRAY = 2,
-	JSMN_STRING = 3,
-	JSMN_PRIMITIVE = 4
-} jsmntype_t;
-
-enum jsmnerr {
-	/* Not enough tokens were provided */
-	JSMN_ERROR_NOMEM = -1,
-	/* Invalid character inside JSON string */
-	JSMN_ERROR_INVAL = -2,
-	/* The string is not a full JSON packet, more bytes expected */
-	JSMN_ERROR_PART = -3
+enum json_res {
+    JSON_CONTINUE = 1,
+    JSON_DONE = 0,
+	JSON_ERROR_PARSING = -1,
+	JSON_ERROR_STORAGE_SIZE = -2,
+    JSON_PARSER_FAULT = -3 //the parser made an illegal action
 };
 
-/**
- * JSON token description.
- * @param		type	type (object, array, string etc.)
- * @param		start	start position in JSON data string
- * @param		end		end position in JSON data string
- */
+enum json_match{
+    JSON_NO_MATCH = -1,
+    JSON_MATCH = -2
+    /* positive values used internally
+     * to represent a match upto a
+     * certain level */
+};
+
 typedef struct {
-	jsmntype_t type;
-	int start;
-	int end;
-	int size;
-#ifdef JSMN_PARENT_LINKS
-	int parent;
-#endif
-} jsmntok_t;
+    const char **key_addr;
+    size_t max_level;
+    int match_upto_level;
+    char *storage;
+    size_t storage_sz;
+    struct json_str_t *next;
+} json_str_t;
 
-/**
- * JSON parser. Contains an array of token blocks available. Also stores
- * the string being parsed now and current position in that string
- */
 typedef struct {
-	unsigned int pos; /* offset in the JSON string */
-	unsigned int toknext; /* next token to allocate */
-	int toksuper; /* superior token node, e.g parent object or array */
-} jsmn_parser;
+    const char **key_addr;
+    size_t max_level;
+    int match_upto_level;
+    int *storage;
+    struct json_int_t *next;
+} json_int_t;
 
-/**
- * Create JSON parser over an array of tokens
- */
-void jsmn_init(jsmn_parser *parser);
 
-/**
- * Run JSON parser. It parses a JSON data string into and array of tokens, each describing
- * a single JSON object.
- */
-int jsmn_parse(jsmn_parser *parser, const char *js, size_t len,
-		jsmntok_t *tokens, unsigned int num_tokens);
+#define JSON_LIST_EMPTY NULL
+
+
+typedef struct {
+	unsigned int level; //current address level
+    size_t buff_offset;
+    size_t key_offset;
+    int state;
+    json_int_t *ints;
+    size_t n_ints;
+    json_str_t *strs;
+    size_t n_strs
+    json_int_t *int_matches;
+    json_str_t *str_matches;
+    bool should_find_matches;
+} json_parser;
+
+void json_ints_init(json_val_int_t *values, const char **keys_addrs[],
+                    int *storages, size_t n_values);
+void json_strs_init(json_val_int_t *values, const char **keys_addrs[],
+                    int *storages, size_t n_values);
+
+void json_parser_init(json_parser *parser, json_int_t *ints, size_t n_ints,
+                      json_str_t *strs, size_t n_strs);
+
+int json_parse(json_parser *parser, const char *input_buff, size_t buff_sz);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __JSMN_H_ */
+#endif /* __JSON_H_ */
